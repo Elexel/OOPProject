17bb10dacb8e00f93e61001fdf90cb48d9ed68a9
Elexel <43592164+Elexel@users.noreply.github.com>
2018-11-23 09:27:21 +0800

    Added Files
    Added Peron.cs, Product.cs, Calculations.cs

---
+++ Calculations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Calculations
+    {
+        public int GetCalculateAge(string BirthDate)
+        {
+            return 0;
+        }
+        public decimal GetCalculateInterest(decimal principalAmount, decimal monthlyRate)
+        {
+            return 0;
+        }
+        public decimal CalculateAccruedAmount(decimal principalAmount, decimal monthlyRate, string date)
+        {
+            return 0;
+        }
+    }
+}
